805ac25894a6c3858f58a0bd70e558fbac498b00
Reshav ji <116823969+Reshavji@users.noreply.github.com>
2023-06-06 16:07:12 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,30 +1,34 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 import '../styles/App.css';
 
+const getData = async () => {
+  const rawData = await fetch('https://jsonplaceholder.typicode.com/todos');
+  const data = await rawData.json();
+  return data;
+};
 
-const getData = async () =>{
-  const rawData = await fetch('https://jsonplaceholder.typicode.com/todos')
-  const data = await rawData.json()
-  return data
-}
-const Loader = () => <div id="loader">Loading...</div>
+const Loader = () => <div id="loader">Loading...</div>;
+
 const App = () => {
-  const [todos,setTodos] = useState([])
+  const [todos, setTodos] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-  useEffect(()=>{
-    getData().then(data=> {
-      setTodos(data)
-     })
-  },[])
+  useEffect(() => {
+    getData().then((data) => {
+      setTodos(data);
+      setLoading(false);
+    });
+  }, []);
+
   return (
     <div id="main">
-      {
-        loading ? <Loader /> :
-      todos.map(todo=> <div id={`todo-${todo.id}`}>{todo.title}</div>)
-      }
+      {loading ? (
+        <Loader />
+      ) : (
+        todos.map((todo) => <div id={`todo-${todo.id}`}>{todo.title}</div>)
+      )}
     </div>
-  )
-}
-
+  );
+};
 
 export default App;
